17558a9d2a8d46296ecf719ca97a4ced3f177c42
reondikapurio <konreo0402@gmail.com>
2026-02-09 12:29:30 +0900

    prisma error解消

---
+++ prisma.config.ts
@@ -0,0 +1,9 @@
+// 👇 これを追加！ これで .env が読み込まれます
+import 'dotenv/config'; 
+import { defineConfig } from '@prisma/config';
+
+export default defineConfig({
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+});
