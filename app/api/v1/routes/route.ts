ca59279f7335a3b6641225e279400afbb7468e3d
reondikapurio <konreo0402@gmail.com>
2026-02-26 19:57:49 +0900

    return 修正

--- app/api/v1/routes/route.ts
+++ app/api/v1/routes/route.ts
@@ -11,7 +11,7 @@ import { PatchRouteSchema } from "@/features/routes/schema";
 // GET /api/v1/routems
 // 最近作成されたルートを一覧返却します
 export async function GET(req: NextRequest) {
-  await handleRequest(async () => {
+  return await handleRequest(async () => {
     const supabase = await createClient(req);
     const { data: { user }, error } = await supabase.auth.getUser();
     const safe_user = error ? null : user;
@@ -27,15 +27,13 @@ export async function GET(req: NextRequest) {
 // ルート作成用のAPI。
 
 export async function POST(req: NextRequest) {
-  await handleRequest(async () => {
+  return await handleRequest(async () => {
     const supabase = await createClient(req);
     const { data: { user }, error } = await supabase.auth.getUser();
     if (error || !user) {
       throw new Error("Unauthorized");
     }
     const body = await req.json();
-
-    //TODO:近藤 bodyのバリデーション書いてない
     if (!body || !Array.isArray(body.items)) {
       throw new Error("Invalid body: items[] is required");
     }
